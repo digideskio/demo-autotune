2cefffeb4224a8504b6f57ffc16f00279451e5e8
KavyaSukumar <Kavya.Sukumar@hotmail.com>
2015-07-23 12:49:11 -0700

    update migrations

--- db/migrate/20150723194829_change_user_meta_format.autotune.rb
+++ db/migrate/20150723194829_change_user_meta_format.autotune.rb
@@ -1,6 +1,6 @@
 # This migration comes from autotune (originally 20150506195022)
 class ChangeUserMetaFormat < ActiveRecord::Migration
   def change
-    Autotune::User.connection.execute('UPDATE autotune_users SET meta = "{}"')
+    Autotune::User.connection.execute("UPDATE autotune_users SET meta = '{}'")
   end
 end
