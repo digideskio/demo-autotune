7c8456b097b489e88f0b784e04e20e627c33e2a9
Ryan Mark <ryan@mrk.cc>
2015-07-23 17:21:07 -0700

    adjust deployment

--- config/deploy.rb
+++ config/deploy.rb
@@ -2,7 +2,7 @@
 lock '3.4.0'
 
 set :application, 'autotune'
-set :repo_url, 'git@example.com:me/my_repo.git'
+set :repo_url, 'git@github.com:voxmedia/demo-autotune.git'
 
 # Default branch is :master
 # ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
