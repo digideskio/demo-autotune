861c2e7311b43b76c0afb5d4240a8b19bf41fe3d
Ryan Mark <ryan@mrk.cc>
2015-07-23 18:14:38 -0700

    favor developer login if no github app keys are present

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,6 +1,6 @@
 OmniAuth.config.logger = Rails.logger
-Rails.configuration.omniauth_preferred_provider = Rails.env.production? ? :github : :developer
+Rails.configuration.omniauth_preferred_provider = (Rails.env.production? && ENV['GITHUB_KEY']) ? :github : :developer
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :developer unless Rails.env.production?
+  provider :developer unless Rails.env.production? && ENV['GITHUB_KEY']
   provider :github, ENV['GITHUB_KEY'], ENV['GITHUB_SECRET']
 end
